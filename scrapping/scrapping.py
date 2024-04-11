3d2a55b12deb6c86980fe389e68be96c3b293a7b
Bruno Melo <brunomelo1315@gmail.com>
2024-04-10 21:19:22 -0300

    add async to scrapping

--- scrapping/scrapping.py
+++ scrapping/scrapping.py
@@ -3,6 +3,24 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+import io
+import asyncio
+
+import aiohttp
+
+
+async def get_csv_async(client, url):
+    async with client.get(url) as response:
+        with io.StringIO(await response.text()) as text_io:
+            return pd.read_csv(text_io, sep=None, engine="python")
+
+
+async def get_all_csvs_async(urls):
+    async with aiohttp.ClientSession() as client:
+        futures = [get_csv_async(client, url) for url in urls]
+        return await asyncio.gather(*futures)
+
+
 base_url = "http://vitibrasil.cnpuv.embrapa.br/download/"
 
 res = requests.get(base_url)
@@ -12,15 +30,17 @@ if res.status_code == 200:
 
     files_href = soup.find_all("a", href=lambda href: href and ".csv" in href.lower())
 
-    if files_href:
-        files_list = [file["href"] for file in files_href if "csv" in file["href"]]
-    else:
-        print("Not Found")
+    files_url = [
+        base_url + file["href"] for file in files_href if "csv" in file["href"]
+    ]
 else:
     print("Error")
 
 
-for file in files_list:
-    df = pd.read_csv(base_url + file, sep=None, engine="python")
+csv_files_list = asyncio.get_event_loop().run_until_complete(
+    get_all_csvs_async(files_url)
+)
 
-    print(df.head())
+
+for csv in csv_files_list:
+    print(csv)
