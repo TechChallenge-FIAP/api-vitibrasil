b1e605821c30e6e4d68d45dbd6e90437496fb82a
Bruno Melo <brunomelo1315@gmail.com>
2024-05-21 22:01:52 -0300

    :bug: fix a bug on pivot cols

--- crud/Producao.py
+++ crud/Producao.py
@@ -3,27 +3,24 @@ import numpy as np
 import sqlite3
 
 df_producao: pd.DataFrame = pd.read_csv(
-    "tmp/Producao.csv", 
-    sep=";", 
-    engine="python", 
-    header=0
+    "tmp/Producao.csv", sep=";", engine="python", header=0
 )
 
-info_cols = 2
+info_cols = 3
 
 years_col = [
     year for year in range(1970, 1970 + (len(df_producao.columns) - info_cols))
 ]
 
-df_columns = ["id", "produto"] + years_col
+df_columns = ["id", "produtos_ext", "produto"] + years_col
 
 df_producao.columns = df_columns
 
 df_unpivot = df_producao.melt(
-    id_vars=["id", "produto"], 
-    value_vars=years_col, 
-    var_name="ano", 
-    value_name="quantidade_l"
+    id_vars=["id", "produto"],
+    value_vars=years_col,
+    var_name="ano",
+    value_name="quantidade_l",
 )
 
 df_unpivot["categoria"] = np.where(
@@ -36,11 +33,11 @@ df_categories = df_unpivot.copy()
 
 df_categories = df_categories[df_categories["produto"].str.isupper()]
 df_categories = df_categories[["id", "categoria", "ano", "quantidade_l"]]
-df_categories['id'] = np.arange(0, len(df_categories))
+df_categories["id"] = np.arange(0, len(df_categories))
 
 df_products = df_unpivot[~df_unpivot["produto"].str.isupper()]
 df_products = df_products[["id", "produto", "categoria", "ano", "quantidade_l"]]
-df_products['id'] = np.arange(0, len(df_products))
+df_products["id"] = np.arange(0, len(df_products))
 
 conn = sqlite3.connect("instance/Database.db")
 
